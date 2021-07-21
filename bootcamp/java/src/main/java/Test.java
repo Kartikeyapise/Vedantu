3e0c9ff5d213985b37608e31e233bc869880d3b7
kartikeya <kartikeya.work@gmail.com>
2021-07-21 16:20:48 +0530

    just for copying data

---
+++ bootcamp/java/src/main/java/Test.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Test {
+
+    public static void main(String[] args) {
+//        System.out.println("jello");
+//        reverseString("hello");
+           final int a;
+           a=1;
+        System.out.println(a);
+    }
+
+//    public static String reverseString(String s) {
+//        String[] sarr = Arrays.split("");
+//        Collections.reverse(Arrays.asList(sarr));
+//        for (String x : sarr) {
+//            System.out.println(x);
+//        }
+//        return s;
+//    }
+}
